76c2e9f1638c8ecec8d5b5feeea3303262429417
Aung Min Khant <aungminkhantcnx@Aungs-MacBook-Air.local>
2026-04-01 21:01:30 +0700

    adding flex

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-    <div>Hello</div>
+      <div className="container">
+        <Header />
+        <Nav />
+        <Main />
+        <Footer />
+      </div>
     </>
   );
 }
